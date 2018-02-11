e7e3c4e009b9a1c234084ad2279cd591418d443a
someone <someone@example.com>
2018-02-11 17:14:48 +0900

    Add list fragment

--- app/src/main/java/com/example/n/simplediary/MainActivity.java
+++ app/src/main/java/com/example/n/simplediary/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.n.simplediary;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Set fragment
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.FragmentContainer, DiaryListFragment.newInstance(0));
+        transaction.commit();
     }
 
     @Override
